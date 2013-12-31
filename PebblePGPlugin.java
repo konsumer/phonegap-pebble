bc85b797e4da38c036b110ee84c2095df0ac435a
David Konsumer <konsumer@jetboystudio.com>
2013-12-31 09:37:39 -0800

    more stub

--- PebblePGPlugin.java
+++ PebblePGPlugin.java
@@ -15,55 +15,125 @@ public class PebblePGPlugin extends CordovaPlugin {
 
         boolean retValue = true;
         if (action.equals("areAppMessagesSupported")) {
-
+            this.areAppMessagesSupported(args);
         } else if (action.equals("closeAppOnPebble")) {
-
+            this.closeAppOnPebble(args);
         } else if (action.equals("customizeWatchApp")) {
-
+            this.customizeWatchApp(args);
         } else if (action.equals("getWatchFWVersion")) {
-
+            this.getWatchFWVersion(args);
         } else if (action.equals("isDataLoggingSupported")) {
-
+            this.isDataLoggingSupported(args);
         } else if (action.equals("isWatchConnected")) {
-
+            this.isWatchConnected(args);
         } else if (action.equals("registerDataLogReceiver")) {
-
+            this.registerDataLogReceiver(args);
         } else if (action.equals("registerPebbleConnectedReceiver")) {
-
+            this.registerPebbleConnectedReceiver(args);
         } else if (action.equals("registerPebbleDisconnectedReceiver")) {
-
+            this.registerPebbleDisconnectedReceiver(args);
         } else if (action.equals("registerReceivedAckHandler")) {
-
+            this.registerReceivedAckHandler(args);
         } else if (action.equals("registerReceivedDataHandler")) {
-
+            this.registerReceivedDataHandler(args);
         } else if (action.equals("registerReceivedNackHandler")) {
-
+            this.registerReceivedNackHandler(args);
         } else if (action.equals("requestDataLogsForApp")) {
-
+            this.requestDataLogsForApp(args);
         } else if (action.equals("sendAckToPebble")) {
-
+            this.sendAckToPebble(args);
         } else if (action.equals("sendDataToPebble")) {
-
+            this.sendDataToPebble(args);
         } else if (action.equals("sendDataToPebbleWithTransactionId")) {
-
+            this.sendDataToPebbleWithTransactionId(args);
         } else if (action.equals("sendNackToPebble")) {
-
+            this.sendNackToPebble(args);
         } else if (action.equals("startAppOnPebble")) {
-
+            this.startAppOnPebble(args);
         } else if (action.equals("addEventListener")) {
-
+            this.addEventListener(args);
         } else if (action.equals("sendAppMessage")) {
-
+            this.sendAppMessage(args);
         } else if (action.equals("showSimpleNotificationOnPebble")) {
-
+            this.showSimpleNotificationOnPebble(args);
         } else if (action.equals("getAccountToken")) {
-
+            this.getAccountToken(args);
         } else if (action.equals("openURL")) {
-
-        }else{
+            this.openURL(args);
+        } else{
             retValue = false;
         }
         
         return retValue;
     }
+
+    private void areAppMessagesSupported(JSONArray args) throws JSONException {
+    }
+
+    private void closeAppOnPebble(JSONArray args) throws JSONException {
+    }
+
+    private void customizeWatchApp(JSONArray args) throws JSONException {
+    }
+
+    private void getWatchFWVersion(JSONArray args) throws JSONException {
+    }
+
+    private void isDataLoggingSupported(JSONArray args) throws JSONException {
+    }
+
+    private void isWatchConnected(JSONArray args) throws JSONException {
+    }
+
+    private void registerDataLogReceiver(JSONArray args) throws JSONException {
+    }
+
+    private void registerPebbleConnectedReceiver(JSONArray args) throws JSONException {
+    }
+
+    private void registerPebbleDisconnectedReceiver(JSONArray args) throws JSONException {
+    }
+
+    private void registerReceivedAckHandler(JSONArray args) throws JSONException {
+    }
+
+    private void registerReceivedDataHandler(JSONArray args) throws JSONException {
+    }
+
+    private void registerReceivedNackHandler(JSONArray args) throws JSONException {
+    }
+
+    private void requestDataLogsForApp(JSONArray args) throws JSONException {
+    }
+
+    private void sendAckToPebble(JSONArray args) throws JSONException {
+    }
+
+    private void sendDataToPebble(JSONArray args) throws JSONException {
+    }
+
+    private void sendDataToPebbleWithTransactionId(JSONArray args) throws JSONException {
+    }
+
+    private void sendNackToPebble(JSONArray args) throws JSONException {
+    }
+
+    private void startAppOnPebble(JSONArray args) throws JSONException {
+    }
+
+    private void addEventListener(JSONArray args) throws JSONException {
+    }
+
+    private void sendAppMessage(JSONArray args) throws JSONException {
+    }
+
+    private void showSimpleNotificationOnPebble(JSONArray args) throws JSONException {
+    }
+
+    private void getAccountToken(JSONArray args) throws JSONException {
+    }
+    
+    private void openURL(JSONArray args) throws JSONException {
+    }
+
 }
