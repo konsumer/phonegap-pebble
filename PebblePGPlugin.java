6ed0717488248eb563c8f03cdfd978d5ca133a3b
David Konsumer <konsumer@jetboystudio.com>
2013-12-31 09:20:40 -0800

    more stub

--- PebblePGPlugin.java
+++ PebblePGPlugin.java
@@ -14,23 +14,56 @@ public class PebblePGPlugin extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
         boolean retValue = true;
-        /*
-        if (action.equals("version")) {
-        this.version();
-        } else if (action.equals("setEnvironment")) {
-        this.setEnvironment(args);
-        } else if (action.equals("environment")) {
-        this.environment();
-        } else if (action.equals("prepareForPayment")) {
-        this.prepareForPayment(args);
-        } else if (action.equals("presentPaymentUI")) {
-        this.presentPaymentUI(args);
-        } else {
-        retValue = false;
-        }
-        */
-        retValue = false;
+        if (action.equals("areAppMessagesSupported")) {
 
+        } else if (action.equals("closeAppOnPebble")) {
+
+        } else if (action.equals("customizeWatchApp")) {
+
+        } else if (action.equals("getWatchFWVersion")) {
+
+        } else if (action.equals("isDataLoggingSupported")) {
+
+        } else if (action.equals("isWatchConnected")) {
+
+        } else if (action.equals("registerDataLogReceiver")) {
+
+        } else if (action.equals("registerPebbleConnectedReceiver")) {
+
+        } else if (action.equals("registerPebbleDisconnectedReceiver")) {
+
+        } else if (action.equals("registerReceivedAckHandler")) {
+
+        } else if (action.equals("registerReceivedDataHandler")) {
+
+        } else if (action.equals("registerReceivedNackHandler")) {
+
+        } else if (action.equals("requestDataLogsForApp")) {
+
+        } else if (action.equals("sendAckToPebble")) {
+
+        } else if (action.equals("sendDataToPebble")) {
+
+        } else if (action.equals("sendDataToPebbleWithTransactionId")) {
+
+        } else if (action.equals("sendNackToPebble")) {
+
+        } else if (action.equals("startAppOnPebble")) {
+
+        } else if (action.equals("addEventListener")) {
+
+        } else if (action.equals("sendAppMessage")) {
+
+        } else if (action.equals("showSimpleNotificationOnPebble")) {
+
+        } else if (action.equals("getAccountToken")) {
+
+        } else if (action.equals("openURL")) {
+
+        }else{
+            retValue = false;
+        }
+        
         return retValue;
     }
 }
--- README.md
+++ README.md
@@ -7,18 +7,18 @@ This is a stub. Still working out details.
 ## Integration
 
 *  Download [Pebble SDK](https://developer.getpebble.com/2/getting-started/) and integrate with project
-*  Add PebblePGPlugin.java to your project, in src/com/paypal/android/sdk/phonegap.
+*  Add PebblePGPlugin.java to your project, in src/com/jetboystudio/pebble/phonegap.
 *  Copy PebblePGPlugin.js to your project's www/js folder.
 *  Add the following to res/xml/config.xml for PhoneGap version 3.0+:
 
 ```xml
 <feature name="Pebble">
-   <param name="android-package" value="com.paypal.android.sdk.phonegap.PebblePGPlugin" />
+   <param name="android-package" value="com.jetboystudio.pebble.phonegap.PebblePGPlugin" />
  </feature>
 ```
 
 for older versions under the plugins tag:
 
 ```xml
-<plugin name="Pebble" value="com.paypal.android.sdk.phonegap.PebblePGPlugin" />
+<plugin name="Pebble" value="com.jetboystudio.pebble.phonegap.PebblePGPlugin" />
 ```
