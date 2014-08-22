a82bc1b5fa86b2b364fe3f32eb2a76efd3b30d46
David Konsumer <konsumer@jetboystudio.com>
2014-08-21 23:08:10 -0700

    simplifying to get it to work

--- src/android/PebblePGPlugin.java
+++ src/android/PebblePGPlugin.java
@@ -14,186 +14,20 @@ public class PebblePGPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext cb) throws JSONException {
-        switch (action) {
-            case "areAppMessagesSupported":
-                cb.success(PebbleKit.areAppMessagesSupported(
-                    getApplicationContext()
-                ));
-                break;
-
-            case "closeAppOnPebble":
-                UUID uuid = UUID.fromString(args.getString(0));
-                cb.success(PebbleKit.closeAppOnPebble(
-                    getApplicationContext(),
-                    uuid
-                ));
-                break;
-
-            case "customizeWatchApp":
-                cb.error("Not Implemented: Need to figure out icon image.");
-                break;
-
-            case "getWatchFWVersion":
-                PebbleKit.FirmwareVersionInfo fw = PebbleKit.getWatchFWVersion(getApplicationContext());
-                JSONObject json = new JSONObject();
-                json.put("version", fw.getMajor() + "." + fw.getMinor() + "." + fw.getPoint());
-                json.put("tag", fw.getTag());
-                cb.success(json);
-                break;
-
-            case "isDataLoggingSupported":
-                cb.success(PebbleKit.isDataLoggingSupported(getApplicationContext()));
-                break;
-
-            case "isWatchConnected":
-                cb.success(PebbleKit.isWatchConnected(getApplicationContext()));
-                break;
-
-            case "registerDataLogReceiver":
-                UUID uuid = UUID.fromString(args.getString(0));
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-                pluginResult.setKeepCallback(true);
-                cb.sendPluginResult(pluginResult);
-                
-                mDataLogReceiver = new PebbleKit.PebbleDataLogReceiver(uuid) {
-                    @Override
-                    public void receiveData(Context context, UUID logUuid, UnsignedInteger timestamp, UnsignedInteger tag, UnsignedInteger secondsSinceEpoch) {
-                        JSONObject json = new JSONObject();
-                        json.put("logUuid", logUuid);
-                        json.put("timestamp", timestamp);
-                        json.put("tag", tag);
-                        json.put("secondsSinceEpoch", secondsSinceEpoch);
-                        cb.success(json);
-                    }
-                };
-                PebbleKit.registerDataLogReceiver(
-                    getApplicationContext(),
-                    mDataLogReceiver
-                );
-                break;
-
-            case "unregisterDataLogReceiver":
-                if (mDataLogReceiver != null) {
-                    unregisterReceiver(mDataLogReceiver);
-                    mDataLogReceiver = null;
-                }
-                cb.success();
-                break;
-
-            case "registerPebbleConnectedReceiver":
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-                pluginResult.setKeepCallback(true);
-                cb.sendPluginResult(pluginResult);
-                
-                PebbleKit.registerPebbleConnectedReceiver(getApplicationContext(), new BroadcastReceiver() {
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-                        cb.success(intent.getClass().getSimpleName());
-                    }
-                });
-                break;
-
-            case "registerPebbleDisconnectedReceiver":
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-                pluginResult.setKeepCallback(true);
-                cb.sendPluginResult(pluginResult);
-
-                PebbleKit.registerPebbleDisconnectedReceiver(getApplicationContext(), new BroadcastReceiver() {
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-                        cb.success(intent.getClass().getSimpleName());
-                    }
-                });
-                break;
-
-            case "registerReceivedAckHandler":
-                UUID uuid = UUID.fromString(args.getString(0));
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-                pluginResult.setKeepCallback(true);
-                cb.sendPluginResult(pluginResult);
-
-                PebbleKit.registerReceivedAckHandler(getApplicationContext(), new PebbleAckReceiver(uuid) {
-                    @Override
-                    public void receiveAck(Context context, int transactionId) {
-                        cb.success(transactionId);
-                    }
-                });
-                break;
-
-            case "registerReceivedDataHandler":
-                cb.error("Not Implemented: unsure of how to convert complex data-types");
-                break;
-
-            case "registerReceivedNackHandler":
-                UUID uuid = UUID.fromString(args.getString(0));
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-                pluginResult.setKeepCallback(true);
-                cb.sendPluginResult(pluginResult);
-
-                PebbleKit.registerReceivedNackHandler(getApplicationContext(), new PebbleAckReceiver(uuid) {
-                    @Override
-                    public void receiveNack(Context context, int transactionId) {
-                        cb.success(transactionId);
-                    }
-                });
-                break;
-
-            case "requestDataLogsForApp":
-                UUID uuid = UUID.fromString(args.getString(0));
-                PebbleKit.requestDataLogsForApp(getApplicationContext(), uuid);
-                cb.success();
-                break;
-
-            case "sendAckToPebble":
-                int transactionId = args.getInt(0);
-                PebbleKit.sendAckToPebble(getApplicationContext(), transactionId);
-                cb.success();
-                break;
-
-            case "sendDataToPebble":
-                cb.error("Not Implemented: unsure of how to convert complex data-types");
-                break;
-
-            case "sendDataToPebbleWithTransactionId":
-                cb.error("Not Implemented: unsure of how to convert complex data-types");
-                break;
-
-            case "sendNackToPebble":
-                int transactionId = args.getInt(0);
-                PebbleKit.sendAckToPebble(getApplicationContext(), transactionId);
-                cb.success();
-                break;
-
-            case "startAppOnPebble":
-                UUID uuid = UUID.fromString(args.getString(0));
-                cb.success(PebbleKit.startAppOnPebble(getApplicationContext(), uuid));
-                break;
-
-            case "addEventListener":
-                cb.error("Not Implemented: this is in the official app javascript.");
-                break;
-
-            case "sendAppMessage":
-                cb.error("Not Implemented: this is in the official app javascript.");
-                break;
-
-            case "showSimpleNotificationOnPebble":
-                cb.error("Not Implemented: this is in the official app javascript.");
-                break;
-
-            case "getAccountToken":
-                cb.error("Not Implemented: this is in the official app javascript.");
-                break;
-
-            case "openURL":
-                cb.error("Not Implemented: this is in the official app javascript.");
-                break;
-            
-            default:
-                return false;
-                break;
+        if (action.equals("getWatchFWVersion")){
+            PebbleKit.FirmwareVersionInfo fw = PebbleKit.getWatchFWVersion(getApplicationContext());
+            JSONObject json = new JSONObject();
+            json.put("version", fw.getMajor() + "." + fw.getMinor() + "." + fw.getPoint());
+            json.put("tag", fw.getTag());
+            cb.success(json);
+            return true;
+        }
+        
+        if (action.equals("isWatchConnected")){
+            cb.success(PebbleKit.isWatchConnected(getApplicationContext()));
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
