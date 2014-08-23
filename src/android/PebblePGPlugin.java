a65ce949c4c3c6a2978858068f78c3852df1b770
David Konsumer <konsumer@jetboystudio.com>
2014-08-22 20:57:00 -0700

    callbacks not working, just removed

--- src/android/PebblePGPlugin.java
+++ src/android/PebblePGPlugin.java
@@ -36,78 +36,31 @@ public class PebblePGPlugin extends CordovaPlugin {
         }
 
         if (action.equals("startAppOnPebble")){
-            String u = args.getString(0);
-            UUID uuid = UUID.fromString(u);
-            cb.success(PebbleKit.startAppOnPebble(
-                this.cordova.getActivity().getApplicationContext(),
-                uuid
-            ));
+            UUID uuid = UUID.fromString(args.getString(0));
+            PebbleKit.startAppOnPebble( this.cordova.getActivity().getApplicationContext(), uuid );
+            cb.success(uuid.toString());
             return true;
         }
 
         if (action.equals("closeAppOnPebble")){
             UUID uuid = UUID.fromString(args.getString(0));
-            cb.success(PebbleKit.closeAppOnPebble(
-                this.cordova.getActivity().getApplicationContext(),
-                uuid
-            ));
+            PebbleKit.closeAppOnPebble( this.cordova.getActivity().getApplicationContext(), uuid );
+            cb.success(uuid.toString());
             return true;
         }
 
         if (action.equals("registerDataLogReceiver")){
-            UUID uuid = UUID.fromString(args.getString(0));
-            mDataLogReceiver = new PebbleKit.PebbleDataLogReceiver(uuid) {
-                @Override
-                public void receiveData(Context context, UUID logUuid, UnsignedInteger timestamp, UnsignedInteger tag, UnsignedInteger secondsSinceEpoch) {
-                    JSONObject json = new JSONObject();
-                    json.put("logUuid", logUuid);
-                    json.put("timestamp", timestamp);
-                    json.put("tag", tag);
-                    json.put("secondsSinceEpoch", secondsSinceEpoch);
-                    cb.success(json);
-                }
-            };
-            PebbleKit.registerDataLogReceiver(
-                this.cordova.getActivity().getApplicationContext(),
-                mDataLogReceiver
-            );
+            cb.error("Not Implemented.");
             return true;
         }
 
         if (action.equals("unregisterDataLogReceiver")){
-            if (mDataLogReceiver != null) {
-                PebbleKit.unregisterReceiver(mDataLogReceiver);
-                mDataLogReceiver = null;
-            }
+            cb.error("Not Implemented.");
             return true;
         }
 
         if (action.equals("customizeWatchApp")){
             cb.error("Not Implemented.");
-            /*
-            // TODO: how should I deal with icon?
-            String jsType = args.getString(0);
-            String name = args.getString(1);
-            int appType = Constants.PebbleAppType.OTHER;
-            UUID uuid = Constants.APP_UUID;
-
-            if (jsType.equals("golf")){
-                appType = Constants.PebbleAppType.GOLF;
-                uuid = Constants.GOLF_UUID;
-            } else if (jsType.equals("sports")) {
-                appType = Constants.PebbleAppType.SPORTS;
-                uuid = Constants.SPORTS_UUID;
-            }
-
-            PebbleKit.customizeWatchApp(
-                this.cordova.getActivity().getApplicationContext(),
-                appType,
-                name,
-                icon
-            );
-
-            cb.success(uuid);
-            */
             return true;
         }
 
