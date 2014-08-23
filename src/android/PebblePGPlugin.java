9245e53e7203ec757c1491edad55066ac1903fd3
David Konsumer <konsumer@jetboystudio.com>
2014-08-22 20:26:49 -0700

    fleshing out more complete API

--- src/android/PebblePGPlugin.java
+++ src/android/PebblePGPlugin.java
@@ -11,17 +11,153 @@ public class PebblePGPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext cb) throws JSONException {
+        if (action.equals("areAppMessagesSupported")){
+            cb.success(PebbleKit.areAppMessagesSupported( this.cordova.getActivity().getApplicationContext() ) ? 1 : 0);
+            return true;
+        }
+
+        if (action.equals("isDataLoggingSupported")){
+            cb.success(PebbleKit.isDataLoggingSupported( this.cordova.getActivity().getApplicationContext() ) ? 1 : 0);
+            return true;
+        }
+
+        if (action.equals("isWatchConnected")){
+            cb.success(PebbleKit.isWatchConnected( this.cordova.getActivity().getApplicationContext() ) ? 1 : 0);
+            return true;
+        }
+
         if (action.equals("getWatchFWVersion")){
-            PebbleKit.FirmwareVersionInfo fw = PebbleKit.getWatchFWVersion( this.cordova.getActivity().getApplicationContext() );
+            PebbleKit.FirmwareVersionInfo fw = PebbleKit.getWatchFWVersion(this.cordova.getActivity().getApplicationContext());
             JSONObject json = new JSONObject();
             json.put("version", fw.getMajor() + "." + fw.getMinor() + "." + fw.getPoint());
             json.put("tag", fw.getTag());
             cb.success(json);
             return true;
         }
-        
-        if (action.equals("isWatchConnected")){
-            cb.success(PebbleKit.isWatchConnected( this.cordova.getActivity().getApplicationContext() ) ? 1 : 0);
+
+        if (action.equals("startAppOnPebble")){
+            String u = args.getString(0);
+            UUID uuid = UUID.fromString(u);
+            cb.success(PebbleKit.startAppOnPebble(
+                this.cordova.getActivity().getApplicationContext(),
+                uuid
+            ));
+            return true;
+        }
+
+        if (action.equals("closeAppOnPebble")){
+            UUID uuid = UUID.fromString(args.getString(0));
+            cb.success(PebbleKit.closeAppOnPebble(
+                this.cordova.getActivity().getApplicationContext(),
+                uuid
+            ));
+            return true;
+        }
+
+        if (action.equals("registerDataLogReceiver")){
+            UUID uuid = UUID.fromString(args.getString(0));
+            mDataLogReceiver = new PebbleKit.PebbleDataLogReceiver(uuid) {
+                @Override
+                public void receiveData(Context context, UUID logUuid, UnsignedInteger timestamp, UnsignedInteger tag, UnsignedInteger secondsSinceEpoch) {
+                    JSONObject json = new JSONObject();
+                    json.put("logUuid", logUuid);
+                    json.put("timestamp", timestamp);
+                    json.put("tag", tag);
+                    json.put("secondsSinceEpoch", secondsSinceEpoch);
+                    cb.success(json);
+                }
+            };
+            PebbleKit.registerDataLogReceiver(
+                this.cordova.getActivity().getApplicationContext(),
+                mDataLogReceiver
+            );
+            return true;
+        }
+
+        if (action.equals("unregisterDataLogReceiver")){
+            if (mDataLogReceiver != null) {
+                PebbleKit.unregisterReceiver(mDataLogReceiver);
+                mDataLogReceiver = null;
+            }
+            return true;
+        }
+
+        if (action.equals("customizeWatchApp")){
+            cb.error("Not Implemented.");
+            /*
+            // TODO: how should I deal with icon?
+            String jsType = args.getString(0);
+            String name = args.getString(1);
+            int appType = Constants.PebbleAppType.OTHER;
+            UUID uuid = Constants.APP_UUID;
+
+            if (jsType.equals("golf")){
+                appType = Constants.PebbleAppType.GOLF;
+                uuid = Constants.GOLF_UUID;
+            } else if (jsType.equals("sports")) {
+                appType = Constants.PebbleAppType.SPORTS;
+                uuid = Constants.SPORTS_UUID;
+            }
+
+            PebbleKit.customizeWatchApp(
+                this.cordova.getActivity().getApplicationContext(),
+                appType,
+                name,
+                icon
+            );
+
+            cb.success(uuid);
+            */
+            return true;
+        }
+
+        if (action.equals("registerPebbleConnectedReceiver")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("registerPebbleDisconnectedReceiver")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("registerReceivedAckHandler")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("registerReceivedDataHandler")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("registerReceivedNackHandler")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("requestDataLogsForApp")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("sendAckToPebble")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("sendDataToPebble")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("sendDataToPebbleWithTransactionId")){
+            cb.error("Not Implemented.");
+            return true;
+        }
+
+        if (action.equals("sendNackToPebble")){
+            cb.error("Not Implemented.");
             return true;
         }
 
--- www/Pebble.js
+++ www/Pebble.js
@@ -2,17 +2,124 @@ var cordova = require('cordova');
 
 var Pebble = {};
 
+/**
+ * Check if Pebble is currently connected
+ * @param  {Function} cb function(error, connected)
+ */
 Pebble.isWatchConnected = function(cb){
-	cordova.exec(function(result){
-		cb(null, result);
-	}, cb, 'Pebble', 'isWatchConnected', []);
-}
+	cordova.exec(function(result){ cb(null, result==1); }, cb, 'Pebble', 'isWatchConnected', []);
+};
 
+/**
+ * Get information about firmware installed on Pebble
+ * @param  {Function} cb function(error, info)
+ */
 Pebble.getWatchFWVersion = function(cb){
-	cordova.exec(function(result){
-		cb(null, result);
-	}, cb, 'Pebble', 'getWatchFWVersion', []);
-}
+	cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'getWatchFWVersion', []);
+};
 
+/**
+ * Find out if app-messages are supported
+ * @param  {Function} cb function(error, supported)
+ */
+Pebble.areAppMessagesSupported = function(cb){
+	cordova.exec(function(result){ cb(null, result==1); }, cb, 'Pebble', 'areAppMessagesSupported', []);
+};
+
+/**
+ * Start an app on the Pebble
+ * @param  {String}   uuid UUID of app to be started
+ * @param  {Function} cb   function(error, uuid)
+ */
+Pebble.startAppOnPebble = function(uuid, cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'startAppOnPebble', [uuid]);
+};
+
+/**
+ * Close an app on the Pebble
+ * @param  {String}   uuid UUID of app to be closed
+ * @param  {Function} cb   function(error, uuid)
+ */
+Pebble.closeAppOnPebble = function(uuid, cb){
+	cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'closeAppOnPebble', [uuid]);
+};
+
+/**
+ * Find out if data-logging is supportedon Pebble
+ * @param  {Function} cb function(error, supported)
+ */
+Pebble.isDataLoggingSupported = function(cb){
+	cordova.exec(function(result){ cb(null, result==1); }, cb, 'Pebble', 'isDataLoggingSupported', []);
+};
+
+/**
+ * Register a callback for data-logging
+ * @param  {String}   uuid UUID of callback
+ * @param  {Function} cb   function(error, data)
+ */
+Pebble.registerDataLogReceiver = function(uuid, cb){
+	cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'registerDataLogReceiver', [uuid]);
+};
+
+/**
+ * Unregister the previously registered receiver
+ * @param  {Function} cb function(error, data)
+ */
+Pebble.unregisterDataLogReceiver = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'unregisterDataLogReceiver', []);
+};
+
+/**
+ * Modify one of the built-in apps
+ * @param  {String}   type "golf", "sports",or somehting else (custom)
+ * @param  {String}   name The name as it appears on watch
+ * @param  {Object}   icon Not really sure yet.  Need to work this out
+ * @param  {Function} cb   function(error, uuid)
+ */
+Pebble.customizeWatchApp = function(type, name, icon, cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'customizeWatchApp', [type, name, icon]);
+};
+
+///////  None of these are implemented:
+
+Pebble.registerPebbleConnectedReceiver = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'registerPebbleConnectedReceiver', []);
+};
+
+Pebble.registerPebbleDisconnectedReceiver = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'registerPebbleDisconnectedReceiver', []);
+};
+
+Pebble.registerReceivedAckHandler = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'registerReceivedAckHandler', []);
+};
+
+Pebble.registerReceivedDataHandler = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'registerReceivedDataHandler', []);
+};
+
+Pebble.registerReceivedNackHandler = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'registerReceivedNackHandler', []);
+};
+
+Pebble.requestDataLogsForApp = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'requestDataLogsForApp', []);
+};
+
+Pebble.sendAckToPebble = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'sendAckToPebble', []);
+};
+
+Pebble.sendDataToPebble = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'sendDataToPebble', []);
+};
+
+Pebble.sendDataToPebbleWithTransactionId = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'sendDataToPebbleWithTransactionId', []);
+};
+
+Pebble.sendNackToPebble = function(cb){
+    cordova.exec(function(result){ cb(null, result); }, cb, 'Pebble', 'sendNackToPebble', []);
+};
 
 module.exports = Pebble;
